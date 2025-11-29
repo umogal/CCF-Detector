c203c5523fd55574a6b3cefabfe643cdfc0b3ce4
umogal <umogal@proton.me>
2025-11-29 21:05:58 +0600

    Fixed bug

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #define VERSION 0.1
 /* Vars */
 long unsigned int digit; // initial user-input
-long unsigned int *ptr_digit = &digit;
+unsigned char confirm;
 int digits[14]; // buffer array to store and inspect and do stuff
 
 /*func prototype*/
@@ -13,10 +13,26 @@ int check_luhn();
 int check_extensive(void);
 
 int main() {
+
+    // banner i.e info
     printf("Basic CCF Detector\n");
     fprintf(stdin, "Version %f\n", VERSION);
     printf("Enter the number:");
+    //take input
     get_input();
+
+    do {
+    printf("\nIs this correct? : %lu\n", digit);
+    printf("y/n?");
+    printf(""); 
+    scanf(" %c", &confirm); // because \n of prev one causes logic bug
+    if (confirm == 'n') {
+     printf("Enter the number:\n");
+     get_input();
+    }
+    }
+    while(confirm == 'n');  
+
     return 0;
 }
 
@@ -27,10 +43,10 @@ int get_input() {
 char line[50];
 fgets(line, sizeof(line), stdin);
 line[strcspn(line, "\n")] = '\0'; // remove new-line
-sscanf(line, "%d", &digit);
+sscanf(line, "%lu", &digit);
 /* digit = atoi(line); */ // using sscanf due for need for extensive input validation
 return 0;
              }
 
-                                
+                             
 
